211aeaa288ca66e9bfc9f97658c846ed640f08bc
ns <6306292+makeitbetter@users.noreply.github.com>
2026-08-01 22:02:51 -0700

    schema edit

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
 import { defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
-import { blogSchema } from 'starlight-blog/schema'
+import { blogSchema } from 'starlight-blog/schema';
+import { pageSiteGraphSchema } from 'starlight-site-graph/schema';
+
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema({extend: (context) => blogSchema(context)}) }),
+	docs: defineCollection({ loader: docsLoader(), schema: docsSchema({extend: (context) => blogSchema(context), extend: pageSiteGraphSchema}) }),
 };
