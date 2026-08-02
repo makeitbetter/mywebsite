191a1ce09697b05d2edb6c3aa94723d171c9e08f
ns <6306292+makeitbetter@users.noreply.github.com>
2026-08-02 00:19:51 -0700

    extra };

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,5 @@ import { pageSiteGraphSchema } from 'starlight-site-graph/schema';
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema({extend: (context) => blogSchema(context)}) }),
         docs: defineCollection({ loader: docsLoader(), schema: docsSchema({extend: pageSiteGraphSchema}) }),
-};
 
 };
